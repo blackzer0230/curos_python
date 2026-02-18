4b9b81f4d813a6fb1029e22021c88679be6e4d86
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-18 19:58:49 -0400

    mi primer commit

---
+++ 02_listas/02_tuplas.py
@@ -0,0 +1,19 @@
+    ###  TUPLAS  ###
+
+# como definir una tupla?
+my_tupla = tuple()
+my_other_tupla = ()
+
+my_tupla = (20, 1.77, "choso", "gonlez")
+print(my_tupla)
+print(type(my_tupla))
+
+# posiciones en tupla XD
+print(my_tupla[0])
+print(my_tupla[-1])
+
+
+
+
+
+
