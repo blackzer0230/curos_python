1f9cf9be4ba97ec79ed8a2db64899a119d9011ca
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-19 00:46:53 -0400

    modificamos los apuntes de tuplas

--- 02_listas/02_tuplas.py
+++ 02_listas/02_tuplas.py
@@ -1,42 +1,64 @@
-    ###  TUPLAS  ###
+         ### TUPLAS ###
+# Una tupla es una estructura de datos similar a una lista, pero inmutable: una vez creada, 
+# no se puede modificar (no se pueden añadir, eliminar ni cambiar elementos). Se usan para datos que no deben cambiar.
 
-# como definir una tupla?
-my_tupla = tuple()
-my_other_tupla = ()
+# Dos formas equivalentes:
+mi_tupla = tuple()          # usando el constructor
+mi_otra_tupla = ()           # usando paréntesis vacíos
 
-my_tupla = (20, 1.77, "choso", "gonlez")
-print(my_tupla)
-print(type(my_tupla))
-
-# posiciones en tupla XD
-
-print(my_tupla[0])
-print(my_tupla[2])
-print(my_tupla[-1])
+# Tupla con elementos
+mi_tupla = (20, 1.77, "choso", "gonlez")
+print(mi_tupla)               # (20, 1.77, 'choso', 'gonlez')
+print(type(mi_tupla))         # <class 'tuple'>
 
 
-# metodos para las tuplas 
+# Acceder a los elementos 
+print(mi_tupla[0])    # 20
+print(mi_tupla[2])    # "choso"
+print(mi_tupla[-1])   # "gonlez"
 
-# cuenta
-print(my_tupla.count("gonlez"))
+# las tuplas solo tienen 2 metodos 
 
-# indice
-print(my_tupla.index("choso"))
+#  cuantas veces aparece un elemento
+print(mi_tupla.count("gonlez"))   # 1
 
-# no me gustam las tuplas es una MIERDAAAAAAA
-
-
-my_tupla = list(my_tupla)
-print(type(my_tupla))
-
-my_tupla[3] = "lovesthef"
-my_tupla.insert(1, "negro")
-print(tuple(my_tupla))
+#  devuelve la posicion de la primera aparicin que aparezca
+print(mi_tupla.index("choso"))    # 2
 
 
-# del my_tupla
-# print(my_tupla) NameError: name 'my_tupla' is not defined
- 
+# las tuplas no se pueden modificar sus elementos 
+# mi_tupla[0] = 30    ❌ Error: TypeError: 'tuple' object does not support item assignment
+
+
+# si se necesita conviar un elemento de una lista solos se tiene que convertir en una lista y despues viseversa
+# Convertir tupla a lista
+mi_lista = list(mi_tupla)
+print(type(mi_lista))          # <class 'list'>
+
+# Modificar la lista
+mi_lista[3] = "lovesthef"
+mi_lista.insert(1, "negro")
+print(mi_lista)                # [20, 'negro', 1.77, 'choso', 'lovesthef']
+
+# Volver a tupla
+mi_nueva_tupla = tuple(mi_lista)
+print(mi_nueva_tupla)          # (20, 'negro', 1.77, 'choso', 'lovesthef')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
