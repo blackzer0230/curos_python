7e01bea790457b5378acdc6d0be71aa1698719b4
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-19 00:37:10 -0400

    modificamos los apuntes de listas

--- 02_listas/01_listas.py
+++ 02_listas/01_listas.py
@@ -1,96 +1,109 @@
-    ### listas ###
+        ### LISTAS ###
+# Una lista es una estructura de datos que permite almacenar múltiples elementos en una sola variable. 
+# Pueden ser de diferentes tipos (enteros, floats, strings, otras listas…) y son mutables, es decir, se pueden modificar después de creadas.
 
-# Una lista se puede hacer asi
-my_list = list( )
-# Esta tambien es otro lista
-my_other_list = []
+# Dos formas equivalentes:
+mi_lista = list()        # usando el constructor
+mi_otra_lista = []       # usando corchetes vacíos
 
-print(len(my_list))
+# Lista con elementos
+numeros = [10, 24, 17, 40, 40, 7, 65]
+datos_persona = [20, 1.80, "Choso", "González"]
 
-
-my_list = [10, 24, 17, 40, 40, 7, 65]
-print(my_list)
-print(len(my_list))
-print(type(my_list))
-
-
-my_other_list = [20, 1.80, "choso", "gonlez"]
-print(my_other_list)
-
-# Como acceder a una lista 
-print(my_other_list[0])
-print(my_other_list[1])
-print(my_other_list[2])
-print(my_other_list[-1])
-print(my_other_list[-3])
-                        # una lista no es una tupla!!!!!
+print(len(numeros))       # 7 → longitud de la lista
+print(type(numeros))      # <class 'list'>
 
 
-# retorno en numero si algo que repite en esa lista
-print(my_other_list.count("choso"))
-print(my_list.count(40))
-
-# Desempaquetar una lista ?
-age, height, alias, surname = my_other_list
-print(surname)
-
-# Se pueden sumar listas?
-print(my_list + my_other_list)
+# Acceder a los elementos
+print(datos_persona[0])    # 20
+print(datos_persona[1])    # 1.8
+print(datos_persona[-1])   # "González" (último)
+print(datos_persona[-3])   # "Choso" (tercero empezando por el final)
 
 
-# python es de tipado dinamico y se cambia mucho
-my_list = "hola choso"
-print(my_list)
-print(type(my_list))
+# Contar apariciones de un elemento
+print(datos_persona.count("Choso"))   # 1
+print(numeros.count(40))              # 2
+
+
+# Desempaquetar una lista
+edad, altura, alias, apellido = datos_persona # debe coincidir el número de variables con la longitud de la lista 
+print(apellido)   # "González"
+
+
+# Concatenar listas
+lista_concatenada = numeros + datos_persona
+print(lista_concatenada)
+
+# Mutabilidad y reasignación
+mi_lista = [1, 2, 3]
+mi_lista = "ahora soy un string"
+print(mi_lista)          # "ahora soy un string"
+
+
+######################
+
+
+# Agrega un elemento al final.
+datos_persona.append("love")
+print(datos_persona)   # [20, 1.8, 'Choso', 'González', 'love']
+
+# Inserta un elemento en una posición específica.
+datos_persona.insert(1, "azul")
+print(datos_persona)   # [20, 'azul', 1.8, 'Choso', 'González', 'love']
+
+# Se puede cambiar directamente un elemento.
+datos_persona[1] = "Rojo"
+print(datos_persona)   # [20, 'Rojo', 1.8, 'Choso', 'González', 'love']
+
+# Elimina la primera aparición del elemento indicado.
+datos_persona.remove("Rojo")
+print(datos_persona)   # [20, 1.8, 'Choso', 'González', 'love']
+
+# Elimina y devuelve el elemento en la posición dada. Si no se pasa índice, elimina el último.
+eliminado = datos_persona.pop()   # elimina 'love'
+print(eliminado)                   # 'love'
+print(datos_persona)               # [20, 1.8, 'Choso', 'González']
+
+# Elimina un elemento por índice o incluso la variable completa.
+del datos_persona[2]               # elimina 'Choso'
+print(datos_persona)               # [20, 1.8, 'González']
+
+# Vacía la lista por completo.
+datos_persona.clear()
+print(datos_persona)               # []
+
+# Ordena la lista de forma ascendente (modifica la original)
+numeros = [6, 5, 3, 2, 1]
+numeros.sort()
+print(numeros)           # [1, 2, 3, 5, 6]
+
+# Invierte el orden de los elementos.
+numeros.reverse()
+print(numeros)           # [6, 5, 3, 2, 1]
+
+
+# revanadas
+mi_lista = [0, 1, 2, 3, 4, 5]
+print(mi_lista[2:4])     # [2, 3]
+print(mi_lista[:3])      # [0, 1, 2]  (desde el inicio hasta índice 2)
+print(mi_lista[3:])      # [3, 4, 5]  (desde índice 3 hasta el final)
+print(mi_lista[-3:])     # [3, 4, 5]  (los tres últimos)
 
 
 
-# como definir una constante en python? no se puede, pero se puede usar mayusculas para indicar que no se debe cambiar
-# CONSTS_PI = 3.1416
-# CONTS_NAME = "choso gonlez"
 
 
-# agregar al final
-my_other_list.append("lovesthe")
-print(my_other_list)
-
-# insertar en una posicion
-my_other_list.insert(1, "azul")
-print(my_other_list)
-
-my_other_list[1] = "Rojo"
-print(my_other_list)
 
 
-# eliminar el primero que vea
-my_other_list.remove("Rojo")
-print(my_other_list)
-
-# eliminar pero quedarse con ese valor
-my_other_list.pop()
-print(my_other_list)
-
-del my_other_list[2]
 
 
-my_new_list = my_other_list.copy()
-
-my_other_list.clear()
-print(my_other_list)
-print(my_new_list)
-
-my_new_list.reverse()
-print(my_new_list)
 
 
-# numerooooooss
-my_num_list = [6, 5, 3, 2, 1]
-my_num_list.sort()
-print(my_num_list)
 
-# explicar revanadas
-my_revanada_list = my_num_list[2 : 4]
-print(my_revanada_list)
+
+
+
 
 
 
--- 02_listas/02_tuplas.py
+++ 02_listas/02_tuplas.py
@@ -9,8 +9,38 @@ print(my_tupla)
 print(type(my_tupla))
 
 # posiciones en tupla XD
+
 print(my_tupla[0])
+print(my_tupla[2])
 print(my_tupla[-1])
+
+
+# metodos para las tuplas 
+
+# cuenta
+print(my_tupla.count("gonlez"))
+
+# indice
+print(my_tupla.index("choso"))
+
+# no me gustam las tuplas es una MIERDAAAAAAA
+
+
+my_tupla = list(my_tupla)
+print(type(my_tupla))
+
+my_tupla[3] = "lovesthef"
+my_tupla.insert(1, "negro")
+print(tuple(my_tupla))
+
+
+# del my_tupla
+# print(my_tupla) NameError: name 'my_tupla' is not defined
+ 
+
+
+
+
 
 
 
