11dc1f367f37cc1a5339afeb7974b28c19b260f5
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-20 21:30:52 -0400

    modificamos los apuntes de tuplas

---
+++ 02_listas/02_sets.py
@@ -0,0 +1,12 @@
+### SETS ###
+my_set = set()
+mi_otro_set = {}
+
+print(type(my_set))
+print(type(mi_otro_set)) #inicialmente dice que s disccionario
+
+
+mi_otro_set = {"choso", "gonlez", 20}
+print(type(mi_otro_set))
+
+
