96a6be79fb2d07a98df95a248f90d5884fe635b1
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-22 14:18:53 -0400

    modificando los apuntes del set

--- 02_listas/02_sets.py
+++ 02_listas/02_sets.py
@@ -1,41 +1,51 @@
-### SETS ###
-my_set = set()
-mi_otro_set = {}
+    ### SET ###
+# Un set es una colección desordenada, mutable y sin elementos duplicados. 
+# Se utilizan cuando la unicidad de los elementos y las operaciones de conjuntos (unión, intersección, etc.)
+# son importantes.
 
-print(type(my_set))
-print(type(mi_otro_set)) #inicialmente dice que es diccionario
+# Dos formas de crear un set vacío
+mi_set = set()               # Correcto: set vacío
+mi_otro_set = {}              # ¡Cuidado! Esto crea un DICCIONARIO vacío, no un set
 
+print(type(mi_set))           # <class 'set'>
+print(type(mi_otro_set))      # <class 'dict'>  (por eso es importante no confundir)
 
+# Para crear un set con elementos, usamos llaves pero con al menos un elemento
 mi_otro_set = {"choso", "gonlez", 20}
-print(type(mi_otro_set)) #ahora cambia a set
+print(type(mi_otro_set))      # Ahora sí es <class 'set'>
+print(len(mi_otro_set))       # 3
+print(mi_otro_set)
 
-print(len(mi_otro_set))
-
-
+# agregar un elemento
 mi_otro_set.add("sthefanny")
+print(mi_otro_set) # el orden puede varias 
 
-print(mi_otro_set) # un set o es una estructura ordenada
+# Intentar agregar un elemento que ya existe
+mi_otro_set.add("choso")
+print(mi_otro_set) # no se agregar porque ya existe el elemento 
 
+# Manera de comprobar que existe
+print("choso" in mi_otro_set)   # True
+print("chosi" in mi_otro_set)   # False
 
-mi_otro_set.add("choso") # un set no admite repetidos 
-print(mi_otro_set)
+# ELIMINAR 
+mi_otro_set.remove("gonlez") # lo elimina, si no existe da error
 
-# para comprobar asi un elemento existe en un set 
-print("choso" in mi_otro_set)
-print("chosi" in mi_otro_set)
-
-
-mi_otro_set.remove("gonlez")
-print(mi_otro_set)
-
-
-mi_otro_set.clear()
-print(mi_otro_set)
-print(len(mi_otro_set))
+mi_otro_set.discard("TeAmo") # lo elimina, si no existe no dara error
 
 
 
 
 
 
-# ventajas del set?
+
+
+
+
+
+
+
+
+
+
+
