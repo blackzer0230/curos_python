26fed95e200ec3305f65e7a2093e104cb4c15ef3
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-22 11:42:47 -0400

    aprendiendo set

--- 02_listas/02_sets.py
+++ 02_listas/02_sets.py
@@ -3,7 +3,7 @@ my_set = set()
 mi_otro_set = {}
 
 print(type(my_set))
-print(type(mi_otro_set)) #inicialmente dice que s disccionario
+print(type(mi_otro_set)) #inicialmente dice que es diccionario
 
 
 mi_otro_set = {"choso", "gonlez", 20}
