b300d5792c7d735b4a28506eb3654fb1f1cd7dd6
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-26 00:17:11 -0400

    todavia por set casi final de clase

--- 02_listas/02_sets.py
+++ 02_listas/02_sets.py
@@ -30,8 +30,64 @@ print("chosi" in mi_otro_set)   # False
 
 # ELIMINAR 
 mi_otro_set.remove("gonlez") # lo elimina, si no existe da error
-
 mi_otro_set.discard("TeAmo") # lo elimina, si no existe no dara error
+
+elementos_eliminados = mi_otro_set.pop() # elimina elementos al azar
+print(elementos_eliminados)
+
+mi_otro_set.clear()
+print(mi_otro_set)
+print(len(mi_otro_set))
+
+
+# convertir un set en una lista y viceversa
+mi_set = {"choso", "gonlez", 20} #set
+mi_lista = list(mi_set) # ahora es una lista
+
+mi_set = set(mi_lista)
+
+# operaciones de conjuntos 
+set1 = {"harry", "gonlez", 20}
+set2 = {"python", "go", "c++"}
+union_set = set1.union(set2)
+print(union_set)
+
+    # tambien se pueden unir usando el operador |
+otra_union_set = set1 | set2
+print(otra_union_set)
+
+
+# intersecciob de elementos comunes
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+interseccion = set_a.intersection(set_b)
+print(interseccion)
+    # o tambien
+otra_interseccio = set_a & set_b
+print(otra_interseccio)
+
+# elmentos que estan en el primen set y no es el otro
+diferencia = set_a.difference(set_b)
+print(diferencia)
+
+# diferencia que estan en uno u otro pero no en ambos 
+sim_diff = set_a.symmetric_difference(set_b)
+print(sim_diff)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
