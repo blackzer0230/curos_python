63cb1190aa9dece2214dd275c81c792347b56d72
blackzer0230 <harry.gonlez0230@gmail.com>
2026-02-28 02:54:35 -0400

    apuntes de diccionarios listos sin extender metodos

--- 02_listas/03_diccionario.py
+++ 02_listas/03_diccionario.py
@@ -24,3 +24,65 @@ otro_dict = {
 
 print(mi_dict)
 print(otro_dict)
+
+
+# acceder a los valores
+print(otro_dict["nombre"])
+# si la clave no existe puede dar un error, para evitar eso podemos usar el metodo .get()
+print(otro_dict.get("email")) # la ultima clave cuenta como valor por defecto (none)
+
+
+# modificar valores existentes
+otro_dict["nombre"] = "choso"
+print(otro_dict["nombre"])
+
+
+# agregar nuevos pares CLAVE:VALOR
+otro_dict["email"] = "prontonmail"
+print(otro_dict)
+
+
+# comprovar si existe
+print("gonzalez" in otro_dict) # gonzalez es un valor no una clave
+print("apellido" in otro_dict)
+
+# eliminar elementos
+del mi_dict["apellido"]
+print(mi_dict)
+
+valor_eliminado = otro_dict.pop("edad")
+print(valor_eliminado)
+
+mi_dict.clear()
+print(mi_dict)
+
+
+# metodos principales mas usados
+print(otro_dict.items()) # devuelve un vista de pares
+
+print(otro_dict.keys()) # devuelve una vista de las claves
+
+print(otro_dict.values()) # devuelve una vista de los valores
+
+# un metodo que es un poco raro pero la ia me lo explico muy bien
+# el fromkeys es un metodo para crear un nuevo diccionario a partir de una secuencia de claves y una valor por defecto(opcional)
+nuevo_dict = dict.fromkeys(["nombre", "edad", "ciudad"])
+print(nuevo_dict) # se crear las claves con valores none por defecto
+nuevo_dict = dict.fromkeys(["nombre", "edad"], "desconocido")
+print(nuevo_dict) # crea las claves con le valor predeterminado "desconocido"
+
+
+# metodos un poco utiles
+otro_dict.update({"pais": "México", "edad": 21})   # edad se actualiza, pais se agrega
+print(otro_dict)
+valor = otro_dict.setdefault("telefono", "000-0000")  # como no existe, lo crea
+print(otro_dict)   # ahora incluye 'telefono': '000-0000'
+
+
+# diccionario anidados 
+usuarios = {
+    "user1": {"nombre": "harry", "edad": 20},
+    "user2": {"nombre": "sthefanny", "edad": 19}
+}
+print(usuarios["user1"]["nombre"])
+print(usuarios["user2"]["edad"])
